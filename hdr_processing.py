dc8bb3b0a20c9ac4482951b2559e67ee6a4127c5
Creepy guy <61487138+richkel@users.noreply.github.com>
2023-12-03 15:02:26 -0500

    Add files via upload

--- hdr_processing.py
+++ hdr_processing.py
@@ -3,50 +3,45 @@ from invokeai.app.invocations.primitives import ImageField, ImageOutput
 import cv2
 import numpy as np
 from PIL import Image
-from invokeai.app.services.image_records.image_records_common import ImageCategory, ResourceOrigin
 import logging
 
 logger = logging.getLogger(__name__)
 
 @invocation("HDR_Processing", title="HDR Image Processing", tags=["image", "hdr"], category="image", version="1.0.0", use_cache=False)
 class HDRProcessingInvocation(BaseInvocation):
-    """
-    A custom node for HDR image processing in InvokeAI.
-    It takes multiple images with different exposure times and merges them into a single HDR image.
-    """
     input_images: list[ImageField] = InputField(description="Input images for HDR processing")
     exposure_times: list[float] = InputField(description="Exposure times for each input image")
 
     def create_hdr_image(self, images, exposure_times):
-        """
-        Processes a list of images and their corresponding exposure times to create an HDR image.
-        """
         images_cv = [cv2.cvtColor(np.array(image), cv2.COLOR_RGB2BGR) for image in images]
-
-        if len(images_cv) != len(exposure_times):
-            raise ValueError("The number of images does not match the number of exposure times.")
-
         alignMTB = cv2.createAlignMTB()
         alignMTB.process(images_cv, images_cv)
-
         merge_debevec = cv2.createMergeDebevec()
         hdr_image = merge_debevec.process(images_cv, times=np.array(exposure_times))
-
         tonemap = cv2.createTonemap(2.2)
         ldr_image = tonemap.process(hdr_image)
         ldr_image = np.clip(ldr_image * 255, 0, 255).astype(np.uint8)
-
-        hdr_processed_image = Image.fromarray(cv2.cvtColor(ldr_image, cv2.COLOR_BGR2RGB))
-        return hdr_processed_image
+        return Image.fromarray(cv2.cvtColor(ldr_image, cv2.COLOR_BGR2RGB))
 
     def invoke(self, context: InvocationContext) -> ImageOutput:
-        """
-        Invokes the HDR processing node. It loads input images, processes them to create an HDR image, 
-        and then returns the result.
-        """
         try:
-            input_pil_images = [context.services.images.get_pil_image(image_field.image_name) for image_field in self.input_images]
-            hdr_result = self.create_hdr_image(input_pil_images, self.exposure_times)
+            input_pil_images = []
+            valid_exposures = []
+
+            for image_field, exposure_time in zip(self.input_images, self.exposure_times):
+                # Replace get_pil_image with the actual method to retrieve the image
+                pil_image = context.services.images.get_pil_image(image_field.image_name)
+                if pil_image is not None:
+                    input_pil_images.append(pil_image)
+                    valid_exposures.append(exposure_time)
+                else:
+                    logger.error(f"Image not found or invalid: {image_field.image_name}")
+
+            if not input_pil_images:
+                logger.error("No valid input images found.")
+                return ImageOutput()
+
+            hdr_result = self.create_hdr_image(input_pil_images, valid_exposures)
 
             output_image = context.services.images.create(
                 image=hdr_result,
@@ -62,9 +57,6 @@ class HDRProcessingInvocation(BaseInvocation):
                 width=output_image.width,
                 height=output_image.height
             )
-        except FileNotFoundError as fnf_error:
-            logger.error(f"File not found: {fnf_error}")
-            return ImageOutput()
         except Exception as e:
             logger.error(f"Unexpected error: {e}")
             raise e
