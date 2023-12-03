ae886bfc4882f470261827063dea9af7a8eaa610
psychedelicious <4822129+psychedelicious@users.noreply.github.com>
2023-12-03 21:01:37 +1100

    fix: fix ExposureTimesInvocation
    Fixes:
    - outputs must inherit from `BaseInvocationOutput`
    - output fields must be created using `OutputField`
    - outputs must use `@invocation_output` decorator to declare their type
    
    Suggested changes:
    - outputs should end in "Output"
    - invocations should end in "Invocation"
    - invocation types should be all lowercase

--- __init__.py
+++ __init__.py
@@ -1,6 +1,6 @@
 # __init__.py in custom_nodes subfolder
 
 
-from .exposuretimesnode import ExposureTimesNode
+from .exposuretimesnode import ExposureTimesInvocation
 from .retrieveimagesfromfile import RetrieveImagesFromFileInvocation
 from .hdr_processing import HDRProcessingInvocation
